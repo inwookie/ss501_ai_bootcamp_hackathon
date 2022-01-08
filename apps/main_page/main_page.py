36683df65ea1b23735549eac9a57f29d7a9674f7
Inwook Baek <inwook@berkeley.edu>
2022-01-08 21:32:19 +0900

    Update main_page.py

--- apps/main_page/main_page.py
+++ apps/main_page/main_page.py
@@ -4,6 +4,8 @@ def app():
     st.write('👆🏻 🔺 ☝ 네비게이션을 사용하여 페이지를 이동하세요.')
     st.title('What We Achieved')
 
+    used_car_prediction = '[🚗  차종 분류](https://share.streamlit.io/inwookie/ss501_ai_bootcamp_hackathon/main/app.py)'
+    st.markdown(used_car_prediction, unsafe_allow_html=True)
     st.markdown("""
     ### 🚗  차종 분류 
     ### 🚓  번호판 인식  
