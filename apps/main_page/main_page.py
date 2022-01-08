f9e1c448aba8dd464bc04f22600f80f074064fec
inwookie <inwook@berkeley.edu>
2022-01-08 18:58:57 +0900

    Final Update

---
+++ apps/main_page/main_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def app():
+    st.title('What We Achieved')
+
+    st.markdown("""
+    ### 🚗  차종 분류 
+    ### 🚓  번호판 인식  
+    ### 🚐  파손 인식  
+    ### 🚙  중고차 가격 예측   
+    """)
